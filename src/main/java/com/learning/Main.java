4a78bcf94de69397b7e317ae7acdd33d27b3256a
alireza <ara.bayat@gmail.com>
2026-07-10 15:59:54 +0330

    add one to one relation

--- src/main/java/com/learning/Main.java
+++ src/main/java/com/learning/Main.java
@@ -6,29 +6,17 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Employee employee = Employee.builder()
+        Employee employee=Employee.builder()
                 .name("alireza")
                 .role(Roles.employee)
-                .birthDate(LocalDate.of(1988, 9, 13))
+                .birthDate(LocalDate.of(1966,9,13))
                 .build();
-
-        AccessCard accessCard = AccessCard.builder()
-                .cardNumber("AC-1001")
-                .issueDate(LocalDate.now())
-                .active(true)
-                .employee(employee)
-                .build();
-
-        employee.setAccessCard(accessCard);
-
-        var factory = Persistence.createEntityManagerFactory("MyPersistenceUnit");
-        EntityManager em = factory.createEntityManager();
-        var transaction = em.getTransaction();
+        var factory= Persistence.createEntityManagerFactory("MyPersistenceUnit");
+        EntityManager em=factory.createEntityManager();
+        var transaction= em.getTransaction();
         transaction.begin();
-        em.persist(accessCard);
         em.persist(employee);
         transaction.commit();
         em.close();
-        factory.close();
     }
 }
---
+++ src/main/java/com/learning/OneToOne.java
@@ -0,0 +1,34 @@
+package com.learning;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import java.time.LocalDate;
+
+public class OneToOne {
+    public static void main(String[] args) {
+        Employee employee = Employee.builder()
+                .name("alireza")
+                .role(Roles.employee)
+                .birthDate(LocalDate.of(1988, 9, 13))
+                .build();
+
+        AccessCard accessCard = AccessCard.builder()
+                .cardNumber("AC-1001")
+                .issueDate(LocalDate.now())
+                .active(true)
+                .employee(employee)
+                .build();
+
+        employee.setAccessCard(accessCard);
+
+        var factory = Persistence.createEntityManagerFactory("MyPersistenceUnit");
+        EntityManager em = factory.createEntityManager();
+        var transaction = em.getTransaction();
+        transaction.begin();
+        em.persist(accessCard);
+        em.persist(employee);
+        transaction.commit();
+        em.close();
+        factory.close();
+    }
+}
